d9667d370a4d9617f40a5ecb80e011bddb18fe8d
DilrukR <68820399+DilrukR@users.noreply.github.com>
2021-09-22 01:38:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// loder
+// const loder = document.getElementById('loder');
+
+// function load() {
+//     loder.style.display = 'none';
+// }
+
+// mobile menu
+const nav = document.getElementById('nav');
+const ham = document.querySelector('.hamburger')
+const cross = document.querySelector('.cross');
+
+ham.addEventListener('click',show);
+cross.addEventListener('click',hide);
+
+function show() {
+    nav.style.transform = 'translatex(0%)';
+    ham.style.display= 'none';
+    cross.style.display ="block";  
+}
+
+function hide() {
+    
+    nav.style.transform = 'translatex(-100%)';
+    cross.style.display="none"
+    ham.style.display= 'block'; 
+      
+}
